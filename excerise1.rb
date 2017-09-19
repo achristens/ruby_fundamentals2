99ef4e4cff6c314949068fb1a4a4b4ec8ca2bac3
Abby Christens <achristens@gmail.com>
2017-09-19 12:33:53 -0400

    Adding movies

---
+++ excerise1.rb
@@ -0,0 +1,6 @@
+documentary = "Garbage Island: An Ocean Full of Plastic"
+drama = "Moonlight"
+comedy = "Why Him"
+dramedy = "The Way Way Back"
+
+puts "Do you enjoy\n1.documentaries\n2.dramas\n3.comedies"
